49eac58d2645e8a6a4feddf7c546a685c8b36ee6
alexx4316 <andresgonzalez25022@gmail.com>
2025-11-22 16:08:11 -0500

    Creacion de la estructura del proyecto Tiketera online

---
+++ src/test/java/com/Events/Tickets/TicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Events.Tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
